e0617c738c4667f2754ed17cdca4d645121e91f6
Deepak Sindhwani <dsindhwani55@gmail.com>
2020-09-05 14:27:51 +0530

    Updated Shop Page with collection preview and collection item Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 //import logo from "./logo.svg";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
