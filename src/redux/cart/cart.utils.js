d9b8f2b93c6e3ddee95a140b0c513fb10a1d2591
Deepak Sindhwani <dsindhwani55@gmail.com>
2020-09-21 13:36:33 +0530

    Added Add and Remove functionality to the checkout item and checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,24 @@ export const addItemToCart = (cartItems, cartItemtoAdd) => {
     return [...cartItems, { ...cartItemtoAdd, quantity: 1 }];
   }
 };
+
+export const clearItemFromCart = (cartItems, { id }) => {
+  return cartItems.filter((cartItem) => cartItem.id !== id);
+};
+
+export const removeItem = (cartItems, cartItemtoRemove) => {
+  let matchedCart = cartItems.find(
+    (cartItem) => cartItem.id === cartItemtoRemove.id
+  );
+
+  console.log("Matched Cart", matchedCart);
+  if (matchedCart.quantity === 1) {
+    return clearItemFromCart(cartItems, cartItemtoRemove);
+  } else {
+    return cartItems.map((cartitem) =>
+      cartitem.id === cartItemtoRemove.id
+        ? { ...cartitem, quantity: cartitem.quantity - 1 }
+        : cartitem
+    );
+  }
+};
